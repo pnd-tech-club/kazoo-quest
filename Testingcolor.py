70f9490a729e778e5ecbcd8c72139ef676ec5506
matthewknecht <1800887@matthewknecht.local>
2015-05-06 13:52:45 -0500

    Forgot to push this when I was messing around with color printing

---
+++ Testingcolor.py
@@ -0,0 +1,21 @@
+color = {
+    'darkwhite':    "\033[1;37m",
+    'darkyellow':   "\033[1;33m",
+    'darkgreen':    "\033[1;32m",
+    'darkblue':     "\033[1;34m",
+    'darkcyan':     "\033[1;36m",
+    'darkred':      "\033[1;31m",
+    'darkmagenta':  "\033[1;35m",
+    'darkblack':      "\033[1;30m",
+    'white':  "\033[0;37m",
+    'yellow': "\033[0;33m",
+    'green':  "\033[0;32m",
+    'blue':   "\033[0;34m",
+    'cyan':   "\033[0;36m",
+    'red':    "\033[0;31m",
+    'magenta':"\033[0;35m",
+    'black':  "\033[0;30m",
+    'off':        "\033[0;0m"
+}
+colorchange = raw_input('> ')
+print color[colorchange]
